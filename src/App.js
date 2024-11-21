28416f298449b8efae055dbe9deaf2518ceedcac
Nole Stites <nole.stites@gmail.com>
2024-11-21 15:42:32 +0900

    Visually-appealing navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './landing_page/navbar';
 
-function MyButton({my_param}) {
-  return (
-    <button>
-      I'm a {my_param}
-    </button>
-  );
-}
-
 function App() {
   return (
-    <div>
+    <div className='background_img'>
       <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyButton my_param={"ButtonParam"}/>
+
       </header>
     </div>
   );
