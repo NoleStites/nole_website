d2ced523f235b2b53a14fa881a08c374cbdc5b5f
NoleStites <nole.stites@gmail.com>
2025-03-19 10:09:53 -0500

    Better sidebar for Minecraft

--- public/Minecraft_Blocks/script.js
+++ public/Minecraft_Blocks/script.js
@@ -85,13 +85,13 @@ function make_flex_grid(columns, rows, texture_size) {
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("mySidebar").style.left = "0px";
     document.getElementById("main").style.marginLeft = "350px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("mySidebar").style.left = "-350px";
     document.getElementById("main").style.marginLeft = "0";
 }
 
